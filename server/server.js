8e02d790a772d3daa79cfe7b04f8ef3923a1b0d4
Manikanta Prasad <manikantaprasad8622@gmail.com>
2025-06-15 10:46:10 +0530

    GreenCart

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['https://greencart-bay.vercel.app/', '']
+const allowedOrigins = ['http://localhost:5173', '']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
